ad37b77648ad0da47106f6ac9a0dbba2b42f867d
Victor Coleman (dfp30) <dfp30@txstate.edu>
2026-04-08 09:12:20 -0500

    Change int to int32_t to guarantee 32 bit
    Also add void reset() to rest the registers.

--- src/RegisterFile.h
+++ src/RegisterFile.h
@@ -3,15 +3,17 @@
 
 #include <array>
 #include <string>
+#include <cstdint>
 
 class RegisterFile {
 public:
     RegisterFile();
 
-    int read(int regNum) const;
+    int32_t read(int regNum) const; //guarentees its 32-bit no matter what
     void write(int regNum, int value);
 
     void dump() const;
+    void reset(); // Resets all registers
 
 private:
     std::array<int, 32> regs;
