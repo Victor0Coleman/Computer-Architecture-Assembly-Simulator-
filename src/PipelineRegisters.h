f91d6aadcba538c08c8ca39975d63ff4512eee4d
Victor Coleman (dfp30) <dfp30@txstate.edu>
2026-04-08 19:01:28 -0500

    Added string type for the different instruc. formats like I,J,R, and nop
    Might contain redundancy in file, I will look back over and remove later.

--- src/PipelineRegisters.h
+++ src/PipelineRegisters.h
@@ -11,17 +11,26 @@ struct IF_ID {
     int pc;                   // the PC value when this instruction was fetched
     bool valid;               // the difference b/w real instruction and false one (NOP)
     IF_ID() : instruction("NOP"), pc(0), valid(false) {}
+
+    void clear() {*this = IF_ID();
+    }
 };
 
 // ─── ID/EX ───────────────────────────────────────────
 // Sits between Decode and Execute
 // ID decodes the instruction and passes everything EX needs here
 struct ID_EX {
+    std::string type;   // instruction type (R, I, J, and "NOP")
+    std::string instruction;  // the raw instruction string e.g. "ADD $t0 $t1 $t2"
+
+    int pc;             // PC value when this instruction was fetched
     // register values read during decode
     int readData1;      // value of $rs
     int readData2;      // value of $rt
 
     int immediate;      // sign-extended immediate value
+    int shamt;          // shift amount for shift instructions
+    int jumpOffset;     // jump offset instruction if needed
 
     // register numbers (needed later for writeback)
     int rs;             // source register number
@@ -29,6 +38,7 @@ struct ID_EX {
     int rd;             // destination register number (R-type)
 
     std::string opcode; // e.g. "ADD", "LW", "BEQ"
+    std::string funct;  // e.g. "ADD", "SUB", "AND"
 
     // control signals
     bool regDst;        // 1 = write to rd, 0 = write to rt
@@ -37,9 +47,10 @@ struct ID_EX {
     bool memWrite;      // 1 = this instruction writes memory (SW)
     bool regWrite;      // 1 = this instruction writes to a register
     bool memToReg;      // 1 = writeback comes from memory, 0 = from ALU
+    bool valid;
 
     ID_EX() : readData1(0), readData2(0), immediate(0),
-              rs(0), rt(0), rd(0), opcode("NOP"),
+              rs(0), rt(0), rd(0),instruction("NOP"), type("NOP"), pc(0), opcode("NOP"),
               regDst(false), aluSrc(false), memRead(false),
               memWrite(false), regWrite(false), memToReg(false) {}
 };
@@ -61,6 +72,8 @@ struct EX_MEM {
     EX_MEM() : aluResult(0), writeData(0), destReg(0),
                memRead(false), memWrite(false),
                regWrite(false), memToReg(false) {}
+
+               void clear() {*this = EX_MEM();}
 };
 
 // ─── MEM/WB ──────────────────────────────────────────
@@ -75,8 +88,12 @@ struct MEM_WB {
     bool regWrite;      // 1 = write result back to register file
     bool memToReg;      // 1 = write memory data, 0 = write ALU result
 
+    bool valid;
+
     MEM_WB() : aluResult(0), readData(0), destReg(0),
                regWrite(false), memToReg(false) {}
+
+    void clear() {*this = MEM_WB();}
 };
 
 #endif
