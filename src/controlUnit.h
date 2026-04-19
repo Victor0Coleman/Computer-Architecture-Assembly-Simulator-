d43c64f2856fc3f9d2eea0475e97391037197602
Victor Coleman (dfp30) <dfp30@txstate.edu>
2026-04-19 10:47:06 -0500

    Files for control unit, h file responsible turning signals off and on

---
+++ src/controlUnit.h
@@ -0,0 +1,15 @@
+#ifndef CONTROL_UNIT_H
+#define CONTROL_UNIT_H
+#include <string>
+#include "PipelineRegisters.h"
+
+//looks at instruction and turns op flow ON/OFF the control signals
+class ControlUnit{
+    static void setsignals(const std::string& op, ID_EX& reg);
+};
+
+#endif // CONTROL_UNIT_H
+
+
+
+
