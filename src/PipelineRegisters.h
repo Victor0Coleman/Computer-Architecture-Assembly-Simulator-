223d4d1b61b5abbc4e44406d266e2f68c438dd4d
Nicholas Krustchinsky <efa26@txstate.edu>
2026-04-25 22:16:29 -0500

    Remove "valid" in idex

--- src/PipelineRegisters.h
+++ src/PipelineRegisters.h
@@ -45,7 +45,7 @@ struct ID_EX {
     bool memWrite;      // 1 = this instruction writes memory (SW)
     bool regWrite;      // 1 = this instruction writes to a register
     bool memToReg;      // 1 = writeback comes from memory, 0 = from ALU
-    bool valid;
+
 
     ID_EX() : readData1(0), readData2(0), immediate(0), type(NOP),
               rs(0), rt(0), rd(0), shamt(0), funct(0), opcode(0),
