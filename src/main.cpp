bf9d27c4f95e16eed166c58c26f9d5b4d32491a7
Nicholas Krustchinsky <efa26@txstate.edu>
2026-05-03 18:45:38 -0500

    Squash of 11 commits: Formatter creating instruction/hex/binary files
    Write goals for new ASM/HEX/Binary representation formatted text files
    add new formatter class to make this possible
    
    empty currently
    
    copy+edit assembler.h to formatter.h
    
    copy assembler.cpp to formatter.cpp
    
    adjust copied assembler code to make formatter
    
    mostly second pass changed
    
    add missing include
    
    include formatter in main.cpp
    
    todo: no binPath
    
    Remove some ol lines in formatter that arent great
    
    make a variable path = argv[1]  in main
    
    + transform formatter output filepath
    
    fix a file extension bug
    
    execute program on every input file, producing 7 formatted "pretty" files
    
    they look quite nice. maybe i should consider retaining the labels ? not sure.

--- src/main.cpp
+++ src/main.cpp
@@ -4,9 +4,11 @@
 #include <string>
 #include "Simulator.h"
 #include "assembler.h"
+#include "formatter.h"
 
 int main(int argc, char* argv[]) {
     std::string binPath = "instructions.bin";
+    std::string formatterDirPath = "input_fancy";
 
     if (argc < 2) {
         std::cout << "Usage: ./mips-sim <input.asm> [-d]\n";
@@ -19,16 +21,34 @@ int main(int argc, char* argv[]) {
         debug = true;
     }
 
+    std::string path = argv[1];
+
     // read the input file line by line
-    std::ifstream file(argv[1]);
+    std::ifstream file(path);
     if (!file.is_open()) {
-        std::cout << "Error: could not open file " << argv[1] << "\n";
+        std::cout << "Error: could not open file " << path << "\n";
         return 1;
     }
 
-    Assembler s;
-    s.assemble(argv[1], binPath);
+    Assembler a;
+    a.assemble(path, binPath);
     std::vector<uint32_t> instructions;
+
+
+    std::string formatOutPath = formatterDirPath;
+
+    {
+        std::size_t slash = path.find_last_of("/\\");
+        std::string filename = slash == std::string::npos ? path : path.substr(slash + 1); 
+        
+        std::size_t dot = filename.find_last_of('.');
+        std::string filenameNoExtension = dot == std::string::npos ? filename : filename.substr(0, dot);
+
+        formatOutPath += "/" + filenameNoExtension + ".txt"; 
+    }
+
+    Formatter f;
+    f.generate(path, formatOutPath);
 
     std::ifstream bin_file(binPath, std::ios::binary);
     if (!bin_file.is_open()) {
